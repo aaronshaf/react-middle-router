52bde76e03f47331eb077a6438bdad6f96ffc4cb
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-10-05 18:09:27 -0600

    docs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react'
 // import BrowserRouter from './lib/Router'
-import Route from './lib/Route'
+import Router from './lib/Router'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hi, Andy.
-        <Route>
+      <div style={{padding: '40px'}}>
+        <h1>react-middle-router</h1>
+        <Router hash='#'>
           <ul>
             <li>
               /
               <ul>
-                <Route>
-                  <Route exclusive pattern='/'>
+                <Router>
+                  <Router exclusive pattern='/'>
                     <li>/</li>
-                  </Route>
-                  <Route exclusive pattern='/foo'>
+                  </Router>
+                  <Router exclusive pattern='/foo'>
                     <li>/foo (exclusive)</li>
-                  </Route>
-                  <Route pattern='/bar'>
+                  </Router>
+                  <Router pattern='/bar'>
                     <li>/bar</li>
-                  </Route>
-                  <Route>
+                  </Router>
+                  <Router>
                     <li>/catchall</li>
-                  </Route>
-                </Route>
+                  </Router>
+                </Router>
               </ul>
             </li>
           </ul>
@@ -41,12 +41,53 @@ class App extends Component {
           <div>
             <a href='/404'>Go to /404</a>
           </div>
-          <Route pattern='/foo'>
+          <Router pattern='/foo'>
             <ul>
               <li>/foo</li>
             </ul>
-          </Route>
-        </Route>
+          </Router>
+        </Router>
+<pre style={{backgroundColor: '#eee', padding: '12px'}}>{`<Router hash='#'>
+  <ul>
+    <li>
+      /
+      <ul>
+        <Router>
+          <Router exclusive pattern='/'>
+            <li>/</li>
+          </Router>
+          <Router exclusive pattern='/foo'>
+            <li>/foo (exclusive)</li>
+          </Router>
+          <Router pattern='/bar'>
+            <li>/bar</li>
+          </Router>
+          <Router>
+            <li>/catchall</li>
+          </Router>
+        </Router>
+      </ul>
+    </li>
+  </ul>
+  <div>
+    <a href='/'>Go to /</a>
+  </div>
+  <div>
+    <a href='/foo'>Go to /foo</a>
+  </div>
+  <div>
+    <a href='/bar'>Go to /bar</a>
+  </div>
+  <div>
+    <a href='/404'>Go to /404</a>
+  </div>
+  <Router pattern='/foo'>
+    <ul>
+      <li>/foo</li>
+    </ul>
+  </Router>
+</Router>
+`}</pre>
       </div>
     )
   }
--- src/lib/Router.js
+++ src/lib/Router.js
@@ -2,29 +2,61 @@ import React from 'react'
 import MiddleRouter from 'middle-router'
 
 export default React.createClass({
-  displayName: 'Match',
+  displayName: 'Route',
 
-  childContextTypes: {
-    router: React.PropTypes.object.isRequired
+  propTypes: {
+    hash: React.PropTypes.string,
+    pattern: React.PropTypes.string,
+    exclusive: React.PropTypes.bool
   },
 
   getInitialState () {
-    return { router: MiddleRouter() }
+    return {
+      router: this.context.router || MiddleRouter({hash: this.props.hash}),
+      matched: false
+    }
   },
 
   getChildContext () {
     return { router: this.state.router }
   },
 
+  contextTypes: {
+    router: React.PropTypes.any
+  },
+
+  childContextTypes: {
+    router: React.PropTypes.any
+  },
+
   componentDidMount () {
-    this.state.router.start()
+    this.state.router.use(this.props.pattern || '/*', this.enter(this.props.exclusive))
+    if (!this.context.router) {
+      this.state.router.start()
+    }
+  },
+
+  enter (exclusive) {
+    return ({params, resolve, next, exiting}) => {
+      if (!this.isMounted()) return
+      this.setState({matched: true})
+      if (exclusive) {
+        resolve()
+      } else {
+        next()
+      }
+      exiting.then(() => {
+        if (!this.isMounted()) return
+        this.setState({matched: false})
+      })
+    }
   },
 
   render () {
-    return <div>{this.props.children}</div>
-  },
-
-  componentWillUnmount () {
-    this.state.router.stop()
+    return (
+      <div>
+        {this.state.matched && this.props.children}
+      </div>
+    )
   }
 })
